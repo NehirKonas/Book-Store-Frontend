32f817cc81f0aa83b023663c7e469161b4916b9e
Nehir Konaş <nehirkonas@gmail.com>
2025-08-29 11:44:26 +0300

    Header with icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,6 @@ export default  function Home(){
         <>
             <h1>Welcome Home!</h1>
             <Link href="/blog">Blog</Link>
-            <Link href="/articles/news?lang=en">Read in English</Link>
-            <Link href="/articles/news?lang=fr">Read in French</Link>
             <Link href="/products">Products</Link>
         </>
     )
